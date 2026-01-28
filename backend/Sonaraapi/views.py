56e5211c730a32611a5d68d629863ce49d1899e9
HolaChris <chrisramos811@gmail.com>
2026-01-27 21:50:07 -0500

    inital commmit of django + Next.js project

---
+++ backend/Sonaraapi/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+# Create your views here.
+@api_view(['GET'])
+def health_check(request):
+    return Response({"ok": True})
